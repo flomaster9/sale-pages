a28215ac17a0d16025fc5ed7987aa5773a9c90b3
Daniel <flomaster9@mail.ru>
2018-03-05 22:16:48 +0300

    Init basic gulp config

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let browserSync = require('browser-sync').create();
+let sourcemaps = require('gulp-sourcemaps');
+let concat = require('gulp-concat');
+let babel = require('gulp-babel');
+
+gulp.task('build-html', function() {
+	gulp.src('./src/html/**/*.html')
+		.pipe(gulp.dest('./dist/html/'));
+});
+
+gulp.task('build-js', function() {
+  gulp.src('./src/scripts/*.js')
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(concat('application.js'))
+		.pipe(gulp.dest('./dist/scripts/'));
+});
+
+gulp.task('build-css', function() {
+    gulp.src('./src/sass/application.scss')
+    	.pipe(sourcemaps.init())
+    	.pipe(sass())
+    	.pipe(sourcemaps.write())
+    	.pipe(gulp.dest('./dist/styles/'));
+});
+
+gulp.task('watcher',['build-css', 'build-html', 'build-js'], function() {
+    gulp.watch('./src/sass/**/*.scss', ['build-css']);
+    gulp.watch('./src/scripts/**/*.js', ['build-js']);
+    gulp.watch('./src/html/**/*.html', ['build-html']);
+    gulp.watch('./src/dist/**/*').on('change', browserSync.reload);
+});
+
+gulp.task('serve',['watcher'], function() {
+    browserSync.init({
+    	server: {
+    		baseDir: './dist',
+        open: true,
+      },
+
+      startPath: './html/index.html'
+    })
+});
